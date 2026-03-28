9f3c5ddc5cde08135b9a2a922d51b0062a86b55d
Abel <ngbandamanabelnoel@gmail.com>
2026-03-28 17:40:22 +0000

    developp commit

--- src/test/java/SeleniumLearning/SeleniumFrameworkLearning/CheckoutTest.java
+++ src/test/java/SeleniumLearning/SeleniumFrameworkLearning/CheckoutTest.java
@@ -3,5 +3,9 @@ package SeleniumLearning.SeleniumFrameworkLearning;
 import SeleniumLearning.SeleniumFrameworkLearning.TestComponents.BaseTest;
 
 public class CheckoutTest extends BaseTest {
+	
+	///gszdvzdzduzd ygyzyuzygz
+	///duudzdtdùd
+	///zdudzdydy test ob fufuy
 
 }
